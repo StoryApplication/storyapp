1ea296189f662f5d88fa6f4187115bc1b1091691
leo <lazucen8@terpmail.umd.edu>
2018-11-14 19:44:42 -0500

    Title Page

--- app/src/main/java/content/MainActivity.java
+++ app/src/main/java/content/MainActivity.java
@@ -1,7 +1,9 @@
 package content;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewTitle = (TextView) findViewById(R.id.title);
+        TextView textViewButton1 = (TextView) findViewById(R.id.button1);
+        TextView textViewButton2 = (TextView) findViewById(R.id.button2);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Kindergarden.ttf");
+        textViewTitle.setTypeface(typeface);
+        textViewButton1.setTypeface(typeface);
+        textViewButton2.setTypeface(typeface);
     }
 }
