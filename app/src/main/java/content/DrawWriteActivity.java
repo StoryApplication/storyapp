0d7ff5bebd15ea7f9a5958656e98452ee13c1b47
Kristine Valenzuela <kvalenz2@terpmail.umd.edu>
2018-11-14 19:55:06 -0500

    .

---
+++ app/src/main/java/content/DrawWriteActivity.java
@@ -0,0 +1,13 @@
+package content;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_write);
+    }
+}
