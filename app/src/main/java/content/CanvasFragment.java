ac875530aab4671f66869b5408d1a441b205a0c2
Ankita Acharya <ankitaacharya23@gmail.com>
2018-12-02 00:07:51 -0500

    more layout with colors

--- app/src/main/java/content/CanvasFragment.java
+++ app/src/main/java/content/CanvasFragment.java
@@ -5,10 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CanvasFragment extends Fragment {
+    Button blue;
+    Button green;
+    Button black;
+    Button purple;
+    Button red;
+    Button orange;
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_draw, container, false);
     }
+
+
 
 }
